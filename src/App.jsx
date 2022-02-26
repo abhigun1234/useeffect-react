419c8c7671a9a1d6423374e6db5f120682351eb2
Abhishek <abhi.ghosh27feb@gmail.com>
2022-02-26 13:01:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Login from "./Login";
+import Register from "./Register";
+import NoMatchPage from "./NoMatchPage";
+import Dashboard from "./Dashboard";
+import { HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <HashRouter>
+      <NavBar />
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/" exact={true} component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="*" component={NoMatchPage} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
